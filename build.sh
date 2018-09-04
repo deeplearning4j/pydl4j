ea47740c58a427053307f2d3f02681a72ed9b493
Max Pumperla <max.pumperla@googlemail.com>
2018-09-04 16:30:29 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp pom.xml $HOME/.pdl4j
+sudo docker build . -t pydl4j
+sudo docker run --mount src="$HOME/.pydl4j",target=/app,type=bind pydl4j
+# this will put uberjars into ~/.pydl4j/target
