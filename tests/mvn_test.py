57c2828476991802969489b1b46516486a6c3c11
farizrahman4u <farizrahman4u@gmail.com>
2018-09-06 00:32:46 +0530

    update tests

---
+++ tests/mvn_test.py
@@ -0,0 +1,36 @@
+import pytest
+import pydl4j import *
+
+
+def test_get_artifacts():
+    artifacts = get_artifacts('datavec')
+    expected = ['datavec-api', 'datavec-local', 'datavec-parent']
+    for e in expected:
+        assert e in artifacts
+
+
+def test_get_versions():
+    versions = get_versions('datavec', 'datavec-api')
+    assert len(versions) >= 12
+
+
+def test_get_latest_version():
+    v = get_latest_version('datavec', 'datavec-api')
+    assert len(v) > 0
+
+
+def test_install():
+    set_context('test')
+    clear_context()
+    mvn_install('datavec', 'datavec-api')
+    mvn_install('datavec', 'datavec-local')
+    assert len(get_jars()) == 2
+    for jar in get_jars():
+        uninstall(jar)
+    assert len(get_jars()) == 0
+    clear_context()
+
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
