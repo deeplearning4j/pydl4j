b46764e9c40a344571d5733e87d3e18aded83e1f
Max Pumperla <max.pumperla@googlemail.com>
2018-09-05 15:58:25 +0200

    init pydl4j folder

--- pydl4j/__init__.py
+++ pydl4j/__init__.py
@@ -7,3 +7,9 @@ else:
     if not os.access(_pydl4j_base_dir, os.W_OK):
         _pydl4j_base_dir = '/tmp'
     _pydl4j_dir = os.path.join(_pydl4j_base_dir, '.pydl4j')
+
+if not os.path.exists(_pydl4j_dir):
+    try:
+        os.makedirs(_pydl4j_dir)
+    except OSError:
+        pass
