3ce9025a897c1da36f02e5cd347d66847b149be4
Fariz Rahman <farizrahman4u@gmail.com>
2018-10-12 22:34:37 +0530

    tiny fix

--- pydl4j/pydl4j.py
+++ pydl4j/pydl4j.py
@@ -31,7 +31,7 @@ def call(arglist):
             arglist.pop(0)
     error = py_call(arglist)
     if error:
-        raise Exception('Subprocess error for command : ' + arglist)
+        raise Exception('Subprocess error for command: ' + list(arglist))
 
 
 _CONFIG_FILE = os.path.join(_MY_DIR, 'config.json')
